4c3c601efeb6e4f724cd9975ef0c35953b640dbd
bisherbh124 <87893728+bisherbh124@users.noreply.github.com>
2021-09-11 03:29:15 -0700

    Add files via upload

---
+++ MyCompiler2/functionSymbol Table.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompiler2
+{
+    class functionSymbol_Table
+    {
+        public List<functionSymbol> SymbolList;
+
+        public functionSymbol_Table()
+        {
+            SymbolList = new List<functionSymbol>();
+        }
+
+        public void addSymbol(functionSymbol s)
+        {
+            SymbolList.Add(s);
+        }
+
+
+
+        public bool searchSymbolByName(string name)
+        {
+            for (int i = 0; i < SymbolList.Count; i++)
+            {
+                if (SymbolList[i].name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+ 
+        public bool searchSymbolbyName3(string name)
+        {
+            for (int i = SymbolList.Count - 1; i >= 0; i--)
+            {
+                if (SymbolList[i].name == name )
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public functionSymbol getSymbolByName(string name)
+        {
+            for (int i = SymbolList.Count - 1; i >= 0; i--)
+            {
+                if (SymbolList[i].name == name)
+                {
+                    return SymbolList[i];
+                }
+            }
+            return null;
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < SymbolList.Count; i++)
+            {
+                SymbolList[i].print();
+            }
+        }
+    }
+}
